b371c4dfa6e5dffc1641ea3b10c86193646d4f3b
DPHamdev <dphamill@gmail.com>
2022-02-03 13:11:23 +0000

    Added datepicker onto booking app, included JS functionality on the base.html and booking app, along with datepicker CDN for bootstrap

--- booking/assets/js/searchFunction.js
+++ booking/assets/js/searchFunction.js
@@ -1,4 +1,14 @@
-// Data Picker Initialization
-$('.datepicker').datepicker({
-    inline: true
+$(document).ready(function(){
+
+  $('.datepicker').datepicker({
+  format: 'dd-mm-yyyy',
+  autoclose: true,
+  startDate: '0d'
+  });
+  
+  $('.cell').click(function(){
+  $('.cell').removeClass('select');
+  $(this).addClass('select');
+  });
+  
   });
